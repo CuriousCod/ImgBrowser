45f0151d0e1111b3fbd8d742e9e570a240f157d7
CuriousCod <69136884+CuriousCod@users.noreply.github.com>
2022-11-26 21:15:41 +0200

    Experimenting with webp support

---
+++ ImgBrowser/src/Helpers/WebpSupport/Utilities.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ImgBrowser.Helpers.WebpSupport
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Copy data from managed to unmanaged memory
+        /// </summary>
+        /// <param name="data">The data you want to copy</param>
+        /// <returns>Pointer to the location of the unmanaged data</returns>
+        public static IntPtr CopyDataToUnmanagedMemory(byte[] data)
+        {
+            // Initialize unmanaged memory to hold the array
+            var size = Marshal.SizeOf(data[0]) * data.Length;
+            var pnt = Marshal.AllocHGlobal(size);
+            // Copy the array to unmanaged memory
+            Marshal.Copy(data, 0, pnt, data.Length);
+            return pnt;
+        }
+
+        /// <summary>
+        /// Get data from unmanaged memory back to managed memory
+        /// </summary>
+        /// <param name="source">A Pointer where the data lifes</param>
+        /// <param name="length">How many bytes you want to copy</param>
+        /// <returns></returns>
+        public static byte[] GetDataFromUnmanagedMemory(IntPtr source, int length)
+        {
+            // Initialize managed memory to hold the array
+            var data = new byte[length];
+            // Copy the array back to managed memory
+            Marshal.Copy(source, data, 0, length);
+            return data;
+        }
+
+        /// <summary>
+        /// Get a file from the disk and copy it into a managed byte array
+        /// </summary>
+        /// <param name="path">The path to the file</param>
+        /// <returns>A byte array containing the file</returns>
+        public static byte[] CopyFileToManagedArray(string path)
+        {
+            // Load file from disk and copy it into a byte array
+            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var data = new byte[fs.Length];
+            fs.Read(data, 0, (int) fs.Length);
+            fs.Close();
+            return data;
+        }
+
+        /// <summary>
+        /// Convert raw image data into a Bitmap object
+        /// </summary>
+        /// <param name="data">The byte array containing the image data</param>
+        /// <param name="imgWidth">The width of your image</param>
+        /// <param name="imgHeight">The height of your image</param>
+        /// <param name="format">The PixelFormat the Bitmap should use</param>
+        /// <returns>The Bitmap object conating you image</returns>
+        public static Bitmap ConvertDataToBitmap(byte[] data, int imgWidth, int imgHeight, PixelFormat format)
+        {
+            var bmp = new Bitmap(imgWidth, imgHeight, format);
+            
+            // Create a BitmapData and Lock all pixels to be written
+            var bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly,
+                bmp.PixelFormat);
+            
+            //Copy the data from the byte array into BitmapData.Scan0
+            Marshal.Copy(data, 0, bmpData.Scan0, data.Length);
+            
+            bmp.UnlockBits(bmpData);
+            
+            return bmp;
+        }
+
+        /// <summary>
+        /// Calculate the needed size for a bitmap
+        /// </summary>
+        /// <param name="imgWidth">The image width</param>
+        /// <param name="imgHeight">The image height</param>
+        /// <param name="format">The pixel format you want to use</param>
+        /// <returns>The bitmap size in bytes</returns>
+        public static int CalculateBitmapSize(int imgWidth, int imgHeight, PixelFormat format)
+        {
+            return imgWidth * imgHeight * Image.GetPixelFormatSize(format) / 8;
+        }
+    }
+}
---
+++ ImgBrowser/src/Helpers/WebpSupport/WebPDecoder.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+// https://github.com/NKnusperer/libwebp-sharp
+
+namespace ImgBrowser.Helpers.WebpSupport
+{
+    public class WebPDecoder
+    {
+        private enum DecodeType
+        {
+            RGB,
+            RGBA,
+            BGR,
+            BGRA,
+            YUV
+        };
+
+        /// <summary>
+        /// The decoder's version number
+        /// </summary>
+        /// <returns>The version as major.minor.revision</returns>
+        public string GetDecoderVersion()
+        {
+            var version = NativeWebPDecoder.WebPGetDecoderVersion();
+            return $"{(version >> 16) & 0xff}.{(version >> 8) & 0xff}.{version & 0xff}";
+        }
+
+        /// <summary>
+        /// Validate the WebP image header and retrieve the image height and width
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="imgWidth">Returns the width of the WebP image</param>
+        /// <param name="imgHeight">Returnsthe height of the WebP image</param>
+        /// <returns>True if the WebP image header is valid, otherwise false</returns>
+        public bool GetInfo(string path, out int imgWidth, out int imgHeight)
+        {
+            var retValue = false;
+            var width = 0;
+            var height = 0;
+            var pnt = IntPtr.Zero;
+
+            try
+            {
+                var data = Utilities.CopyFileToManagedArray(path);
+                pnt = Utilities.CopyDataToUnmanagedMemory(data);
+                var ret = NativeWebPDecoder.WebPGetInfo(pnt, (uint) data.Length, ref width, ref height);
+                if (ret == 1)
+                {
+                    retValue = true;
+                }
+            }
+            finally
+            {
+                // Free the unmanaged memory.
+                Marshal.FreeHGlobal(pnt);
+            }
+
+            imgWidth = width;
+            imgHeight = height;
+            return retValue;
+        }
+
+        /// <summary>
+        /// Decode the WebP image into a RGB Bitmap
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <returns>A Bitmap object with the decoded WebP image.
+        /// Note that a Bitmap object use the BGR format, so if you display the Bitmap in a picturebox red and blue are mixed up</returns>
+        public static Bitmap DecodeRGB(string path)
+        {
+            return Decode(path, DecodeType.RGB, PixelFormat.Format24bppRgb);
+        }
+
+        /// <summary>
+        /// Decode the WebP image into a RGBA Bitmap
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <returns>A Bitmap object with the decoded WebP image.
+        /// Note that a Bitmap object use the ABGR format, so if you display the Bitmap in a picturebox red and blue are mixed up</returns>
+        public static Bitmap DecodeRGBA(string path)
+        {
+            return Decode(path, DecodeType.RGBA, PixelFormat.Format32bppArgb);
+        }
+
+        /// <summary>
+        /// Decode the WebP image into a BGR Bitmap
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <returns>A Bitmap object with the decoded WebP image</returns>
+        public static Bitmap DecodeBGR(string path)
+        {
+            return Decode(path, DecodeType.BGR, PixelFormat.Format24bppRgb);
+        }
+
+        /// <summary>
+        /// Decode the WebP image into a BGRA Bitmap
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <returns>A Bitmap object with the decoded WebP image</returns>
+        public static Bitmap DecodeBGRA(string path)
+        {
+            return Decode(path, DecodeType.BGRA, PixelFormat.Format32bppArgb);
+        }
+
+        /// <summary>
+        /// Decode the WebP image file into raw RGB image data
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="imgWidth">Returns the width of the WebP image</param>
+        /// <param name="imgHeight">Returns the height of the WebP image</param>
+        /// <returns>A byte array containing the raw decoded image data</returns>
+        public static byte[] DecodeRGB(string path, out int imgWidth, out int imgHeight)
+        {
+            return Decode(path, DecodeType.RGB, PixelFormat.Format24bppRgb, out imgWidth, out imgHeight);
+        }
+
+        /// <summary>
+        /// Decode the WebP image file into raw RGBA image data
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="imgWidth">Returns the width of the WebP image</param>
+        /// <param name="imgHeight">Returns the height of the WebP image</param>
+        /// <returns>A byte array containing the raw decoded image data</returns>
+        public static byte[] DecodeRGBA(string path, out int imgWidth, out int imgHeight)
+        {
+            return Decode(path, DecodeType.RGBA, PixelFormat.Format32bppArgb, out imgWidth, out imgHeight);
+        }
+
+        /// <summary>
+        /// Decode the WebP image file into raw BGR image data
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="imgWidth">Returns the width of the WebP image</param>
+        /// <param name="imgHeight">Returns the height of the WebP image</param>
+        /// <returns>A byte array containing the raw decoded image data</returns>
+        public static byte[] DecodeBGR(string path, out int imgWidth, out int imgHeight)
+        {
+            return Decode(path, DecodeType.BGR, PixelFormat.Format24bppRgb, out imgWidth, out imgHeight);
+        }
+
+        /// <summary>
+        /// Decode the WebP image file into raw BGRA image data
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="imgWidth">Returns the width of the WebP image</param>
+        /// <param name="imgHeight">Returns the height of the WebP image</param>
+        /// <returns>A byte array containing the raw decoded image data</returns>
+        public static byte[] DecodeBGRA(string path, out int imgWidth, out int imgHeight)
+        {
+            return Decode(path, DecodeType.BGRA, PixelFormat.Format32bppArgb, out imgWidth, out imgHeight);
+        }
+
+        /// <summary>
+        /// Internal convert method to get a Bitmap from a WebP image file
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="type">The color type you want to convert to</param>
+        /// <param name="format">The PixelFormat the Bitmap should use</param>
+        /// <returns></returns>
+        private static Bitmap Decode(string path, DecodeType type, PixelFormat format)
+        {
+            var data = Decode(path, type, format, out var width, out var height);
+            return Utilities.ConvertDataToBitmap(data, width, height, format);
+        }
+
+        /// <summary>
+        /// Internal convert method to get a byte array from a WebP image file
+        /// </summary>
+        /// <param name="path">The path to the WebP image file</param>
+        /// <param name="type">The color type you want to convert to</param>
+        /// <param name="format">The PixelFormat you want to use</param>
+        /// <param name="imgWidth">Returns the width of the WebP image</param>
+        /// <param name="imgHeight">Returns the height of the WebP image</param>
+        /// <returns></returns>
+        private static byte[] Decode(string path, DecodeType type, PixelFormat format, out int imgWidth,
+            out int imgHeight)
+        {
+            var width = 0;
+            var height = 0;
+            var data = IntPtr.Zero;
+            var outputBuffer = IntPtr.Zero;
+            var result = IntPtr.Zero;
+
+            try
+            {
+                // Load data
+                var managedData = Utilities.CopyFileToManagedArray(path);
+
+                // Copy data to unmanaged memory
+                data = Utilities.CopyDataToUnmanagedMemory(managedData);
+
+                // Get image width and height
+                NativeWebPDecoder.WebPGetInfo(data, (uint) managedData.Length, ref width, ref height);
+
+                // Get image data lenght
+                var dataSize = (uint) managedData.Length;
+
+                // Calculate bitmap size for decoded WebP image
+                var outputBufferSize = Utilities.CalculateBitmapSize(width, height, format);
+
+                // Allocate unmanaged memory to decoded WebP image
+                outputBuffer = Marshal.AllocHGlobal(outputBufferSize);
+
+                // Calculate distance between scanlines
+                var outputStride = width * Image.GetPixelFormatSize(format) / 8;
+
+                // Convert image
+                switch (type)
+                {
+                    case DecodeType.RGB:
+                        result = NativeWebPDecoder.WebPDecodeRGBInto(data, dataSize,
+                            outputBuffer, outputBufferSize, outputStride);
+                        break;
+                    case DecodeType.RGBA:
+                        result = NativeWebPDecoder.WebPDecodeRGBAInto(data, dataSize,
+                            outputBuffer, outputBufferSize, outputStride);
+                        break;
+                    case DecodeType.BGR:
+                        result = NativeWebPDecoder.WebPDecodeBGRInto(data, dataSize,
+                            outputBuffer, outputBufferSize, outputStride);
+                        break;
+                    case DecodeType.BGRA:
+                        result = NativeWebPDecoder.WebPDecodeBGRAInto(data, dataSize,
+                            outputBuffer, outputBufferSize, outputStride);
+                        break;
+                }
+
+                // Set out values
+                imgWidth = width;
+                imgHeight = height;
+
+                // Copy data back to managed memory and return
+                return Utilities.GetDataFromUnmanagedMemory(result, outputBufferSize);
+            }
+            finally
+            {
+                // Free unmanaged memory
+                Marshal.FreeHGlobal(data);
+                Marshal.FreeHGlobal(outputBuffer);
+            }
+        }
+    }
+}
